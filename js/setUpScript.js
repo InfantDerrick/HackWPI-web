0b60efae0df4925f40f366812c7d9516f1ed91a7
Infant Derrick Gnanasusairaj <infant.derrick.gnanasusairaj@gmail.com>
2019-01-19 10:45:36 -0500

    changed from full time listener to sometime listerner

--- js/setUpScript.js
+++ js/setUpScript.js
@@ -1,25 +1,24 @@
-var flag = false;
 function setUp(){
-  flag = true;
-}
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user && user.displayName == null && flag) {
-      var displayName = document.getElementById('displayName').value;
-      var imgUrl = document.getElementById('imgUrl').value;
-      var deviceNumber = parseInt(document.getElementById('deviceNumber').value);
-      console.log(displayName);
-      firebase.auth().currentUser.updateProfile({
-        displayName: displayName,
-        photoURL: imgUrl
-      }).then(function updateData() {
-        firebase.database().ref('users').set({
-          displayName: {
-            "deviceNumber": deviceNumber
-          }
-        });
-        window.open('./login.html', '_self');
-    }).catch(function(error){
-      console.log(error);
+  var user = firebase.auth().currentUser;
+
+if (user && user.displayName == null ) {
+  var displayName = document.getElementById('displayName').value;
+  var imgUrl = document.getElementById('imgUrl').value;
+  var deviceNumber = parseInt(document.getElementById('deviceNumber').value);
+  console.log(displayName);
+  firebase.auth().currentUser.updateProfile({
+    displayName: displayName,
+    photoURL: imgUrl
+  }).then(function updateData() {
+    firebase.database().ref('users').set({
+      displayName: {
+        "deviceNumber": deviceNumber
+      }
     });
-  }
-  });
+    window.open('./login.html', '_self');
+}).catch(function(error){
+  console.log(error);
+});
+} else {
+}
+}
