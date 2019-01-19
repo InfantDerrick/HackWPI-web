6e44d360345d96fc5d643f9ae8c76d677affadae
Infant Derrick Gnanasusairaj <infant.derrick.gnanasusairaj@gmail.com>
2019-01-19 07:21:54 -0500

    authentication update

---
+++ js/firebaseInit.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB1riowoCEt--Iw_yGE17eiV_JjHQF8l7c",
+    authDomain: "soteria-589ca.firebaseapp.com",
+    databaseURL: "https://soteria-589ca.firebaseio.com",
+    projectId: "soteria-589ca",
+    storageBucket: "soteria-589ca.appspot.com",
+    messagingSenderId: "586211282769"
+  };
+  firebase.initializeApp(config);
--- js/script.js
+++ js/script.js
@@ -130,3 +130,28 @@ function epochConverter(epochTime){
 function kelvinToFaren(kelvin){
   return ((kelvin-273.15)*1.8)+32
 }
+function login(){
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  firebase.auth().signInWithEmailAndPassword(username, password).catch(function(error) {
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  window.alert(error);
+});
+}
+function register(){
+  username = document.getElementById("username").value;
+  password1 = document.getElementById("password1").value;
+  password2 = document.getElementById("password2").value;
+  if(password1 != password2){
+    alert("The passwords do not match");
+  }else{
+    firebase.auth().createUserWithEmailAndPassword(username, password1).catch(function(error) {
+
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert(error);
+  });
+  console.log("ASDasd00");
+}
+}
